54252752aed6d14b803ac49988537e37d3227c7b
Joseph Blubaugh <JestonBlu@users.noreply.github.com>
2017-04-24 14:39:46 -0500

    Add files via upload

--- Taxi/random_effects_plots.R
+++ Taxi/random_effects_plots.R
@@ -1,6 +1,5 @@
 rm(list = ls())
 
-library(sas7bdat)
 library(plyr)
 library(reshape)
 library(ggplot2)
@@ -12,8 +11,10 @@ library(gridExtra)
 load("data/prepped_cab_data.rda")
 
 
-dta = read.sas7bdat("analysis/randomeffectsreduced.sas7bdat")
+dta = read.csv("Taxi/randomeffects.csv")
 dta = arrange(dta, desc(Estimate))
+
+dta$Effect = casefold(dta$Effect)
 
 pick.up = subset(dta, Effect == 'pickup_location_id')
 drop.off = subset(dta, Effect == 'dropoff_location_id')
@@ -36,8 +37,8 @@ pick.up$pickup.sig = ifelse(pick.up$pickup_Probt < .05, "Y", "N")
 drop.off$dropoff.sig = ifelse(drop.off$dropoff_Probt < .05, "Y", "N")
 
 
-
-
+pick.up = arrange(pick.up, pickup_estimate)
+drop.off = arrange(drop.off, dropoff_estimate)
 
 ## Move map to the right to cover the data area
 nyc2 = get_map(location = c(lon = -73.9, lat = 40.75), zoom = 11, color = "bw")
@@ -78,7 +79,8 @@ g1 = ggmap(nyc2) +
   geom_point(aes(x = pickup_longitude, y = pickup_latitude, color = pickup_estimate), alpha = .02,
              data = subset(cab.final, pickup.sig == 'Y')) +
   geom_point(aes(x = pickup_lon_cen, y = pickup_lat_cen), size = 1, data = subset(pick.up, pickup.sig == 'Y')) +
-  scale_color_gradient2("", low = "#b30101", midpoint = 0, mid = "#eec900", high = "#118b45", labels = percent) +
+  scale_color_gradient2("", low = "#b30101", midpoint = 0, mid = "#eec900", high = "#118b45", 
+                        labels = percent, breaks = seq(-.05, .15, .05)) +
   ggtitle("Tip Effect by Pickup Location", subtitle = "Significant Effects Only") +
   theme(plot.title = element_text(hjust = .5),
         plot.subtitle = element_text(hjust = .5),
@@ -91,8 +93,9 @@ g2 = ggmap(nyc2) +
   geom_point(aes(x = dropoff_longitude, y = dropoff_latitude, color = dropoff_estimate), alpha = .02,
              data = subset(cab.final, dropoff.sig == 'Y')) +
   geom_point(aes(x = dropoff_lon_cen, y = dropoff_lat_cen), size = 1, data = subset(drop.off, dropoff.sig == 'Y')) +
-  scale_color_gradient2("", low = "#b30101", midpoint = 0, mid = "#eec900", high = "#118b45", labels = percent) +
-  ggtitle("Tip Effect by Dropoff Location", subtitle = "Significant Effects Only") +
+  scale_color_gradient2("", low = "#b30101", midpoint = 0, mid = "#eec900", high = "#118b45", 
+                        labels = percent, breaks = seq(-.05, .15, .05)) +
+  ggtitle("Tip Effect by Drop Off Location", subtitle = "Significant Effects Only") +
   theme(plot.title = element_text(hjust = .5),
         plot.subtitle = element_text(hjust = .5),
         axis.text = element_blank(),
@@ -100,14 +103,15 @@ g2 = ggmap(nyc2) +
         axis.ticks = element_blank())
 
 
-nyc3 = get_map(location = c(lon = -73.98, lat = 40.74), zoom = 13, color = "bw")
+nyc3 = get_map(location = c(lon = -73.98, lat = 40.78), zoom = 12, color = "bw")
 
 
 g3 = ggmap(nyc3) +
   geom_point(aes(x = pickup_longitude, y = pickup_latitude, color = pickup_estimate), alpha = .02,
              data = subset(cab.final, pickup.sig == 'Y')) +
   geom_point(aes(x = pickup_lon_cen, y = pickup_lat_cen), size = 2, data = subset(pick.up, pickup.sig == 'Y')) +
-  scale_color_gradient2("", low = "#b30101", midpoint = 0, mid = "#eec900", high = "#118b45", labels = percent) +
+  scale_color_gradient2("", low = "#b30101", midpoint = 0, mid = "#eec900", high = "#118b45", 
+                        labels = percent, breaks = seq(-.05, .15, .05)) +
   ggtitle("Tip Effect by Pickup Location", subtitle = "Manhattan Area: Significant Effects Only") +
   theme(plot.title = element_text(hjust = .5),
         plot.subtitle = element_text(hjust = .5),
@@ -119,16 +123,41 @@ g4 = ggmap(nyc3) +
   geom_point(aes(x = dropoff_longitude, y = dropoff_latitude, color = dropoff_estimate), alpha = .02,
              data = subset(cab.final, dropoff.sig == 'Y')) +
   geom_point(aes(x = dropoff_lon_cen, y = dropoff_lat_cen), size = 2, data = subset(drop.off, dropoff.sig == 'Y')) +
-  scale_color_gradient2("", low = "#b30101", midpoint = 0, mid = "#eec900", high = "#118b45", labels = percent) +
-  ggtitle("Tip Effect by Dropoff Location", subtitle = "Manhattan Area: Significant Effects Only") +
+  scale_color_gradient2("", low = "#b30101", midpoint = 0, mid = "#eec900", high = "#118b45", 
+                        labels = percent, breaks = seq(-.05, .15, .05)) +
+  ggtitle("Tip Effect by Drop Off Location", subtitle = "Manhattan Area: Significant Effects Only") +
   theme(plot.title = element_text(hjust = .5),
         plot.subtitle = element_text(hjust = .5),
         axis.text = element_blank(),
         axis.title = element_blank(),
         axis.ticks = element_blank())
 
-
 grid.arrange(g1, g2, nrow = 1)
-
 grid.arrange(g3, g4, nrow = 1)
+
+
+
+
+#############################
+## Dropoff time
+
+time = subset(dta, Effect == 'dropoff_time', c("dropoff_time", "Estimate", "Stderrpred",
+                                               "Tvalue", "Probt", "DF"))
+time$dropoff_time = as.numeric(time$dropoff_time)
+time$sig = ifelse(time$Probt < .05, "Y", "N")
+time$LCL = with(time, Estimate - Stderrpred * qt(.975, DF))
+time$UCL = with(time, Estimate + Stderrpred * qt(.975, DF))
+
+
+
+ggplot(time, aes(x = dropoff_time, y = Estimate)) +
+  geom_hline(yintercept = 0, lty = 2) +
+  geom_pointrange(aes(ymin = LCL, ymax = UCL, color = sig)) +
+  scale_color_discrete("Significant") +
+  scale_y_continuous("Estimate", labels = percent) +
+  scale_x_continuous("Dropoff Time") +
+  ggtitle("Drop Off Time Effects by Hour") +
+  theme(plot.title = element_text(hjust = .5))
+
+
 
